b4089bef9db10cf7952a7da78d63be37ad317032
Jessica Rowland <rowlandjl@vcu.edu>
2019-10-21 20:57:12 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,10 @@
 class Dog 
   attr_accessor :name, :breed, :age 
   
-  def initialize(attributes)
-    attributes.each do |key, value|
-      self.send(("#{key}="), value)
-    end 
+  def initialize(name, breed, age)
+     @name = name 
+     @breed = breed 
+     @age = age 
   end 
   
 end 
